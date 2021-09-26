a12e8209cea27e12e3c5db5aac0655ff1ded63c3
Daniel Vega <drvega7@icloud.com>
2021-09-25 21:19:05 -0500

    Initial Commit

---
+++ Gesture Recognizer Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gesture Recognizer Notes
+//
+//  Created by Daniel Vega on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gesture Recognizer NotesTests/Gesture_Recognizer_NotesTests.swift
@@ -0,0 +1,33 @@
+//
+//  Gesture_Recognizer_NotesTests.swift
+//  Gesture Recognizer NotesTests
+//
+//  Created by Daniel Vega on 9/25/21.
+//
+
+import XCTest
+@testable import Gesture_Recognizer_Notes
+
+class Gesture_Recognizer_NotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
