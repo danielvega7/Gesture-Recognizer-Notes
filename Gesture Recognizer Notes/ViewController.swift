5c3fe4d75e6d2b97eecaae5cfe9b968448520bc4
Daniel Vega <drvega7@icloud.com>
2021-09-26 20:57:25 -0500

    finished notes

--- Gesture Recognizer Notes/ViewController.swift
+++ Gesture Recognizer Notes/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textFieldOutlet.delegate = self
     }
 
+    
+    @IBAction func tapScreenAction(_ sender: UITapGestureRecognizer) {
+        view.backgroundColor = UIColor.red
+      var tappedLoc = sender.location(in: view)
+        labelOutlet.center = tappedLoc
+        
+      
+    }
+    
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        var pannedLoc = sender.location(in: view)
+        imageOutlet.center = pannedLoc
+    }
+    @IBAction func removeKeyboardTapped(_ sender: UITapGestureRecognizer) {
+        textFieldOutlet.resignFirstResponder()
+    }
+    
 
 }
 
